99f2d8fb8d011f88a434c55cc38a5c5cd4a74677
Narges-nazeri <narges.naz07@gmail.com>
2023-05-22 10:17:59 +0200

    ändringar

--- src/imat/MainViewController.java
+++ src/imat/MainViewController.java
@@ -14,7 +14,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -35,6 +35,104 @@ public class MainViewController implements Initializable, ShoppingCartListener {
     @FXML
     private FlowPane varukorgFlowPane;
 
+    @FXML
+    private Button betala_button;
+    @FXML
+    private Button personuppgifter_button;
+
+    @FXML
+    private Button Imat_button;
+    @FXML
+    private Button Leveransdetaljer;
+
+    @FXML
+    private Button betalning;
+
+    @FXML
+    private Label Name;
+
+    @FXML
+    private Label efternamn;
+
+    @FXML
+    private  Label Adress;
+
+    @FXML
+    private  Label post_adress;
+
+    @FXML
+    private Label telefonnummer;
+
+    @FXML
+    private Label personnummer;
+
+    @FXML
+    private Label postnummer;
+
+    @FXML
+    private Label Ort;
+
+    @FXML
+    private Button tillbaka_till_startsida;
+
+    @FXML
+    private Button spara_uppgifter;
+
+    @FXML
+    private Button stäng_knappen;
+
+    @FXML
+    private DatePicker Leverans_datum;
+
+    @FXML
+    private RadioButton Lämnas_vid_dörr;
+
+    @FXML
+    private RadioButton Lämnas_till_kund;
+
+    @FXML
+    private Button gå_till_nästa_steg;
+
+    @FXML
+    private Label Skriv_adress;
+
+    @FXML
+    private Label dag;
+
+    @FXML
+    private Button bekräfta_tid;
+
+    @FXML
+    private Button Tillbaka;
+
+    @FXML
+    private Label kortägare;
+
+    @FXML
+    private Label gilltighets_datum;
+
+    @FXML
+    private Label cvv_kod;
+
+    @FXML
+    private RadioButton kort;
+
+    @FXML
+    private RadioButton faktura;
+
+    @FXML
+    private RadioButton swish;
+
+    @FXML
+    private Label total_summa;
+
+    @FXML
+    private Button bekräfta_köp;
+
+    @FXML
+    private Button fortsätt_till_kassan;
+
+
     private Model model = Model.getInstance();
     @FXML
     private Button tillbaka_varukorg_button;
@@ -45,27 +143,56 @@ public class MainViewController implements Initializable, ShoppingCartListener {
     @FXML
     private ListView shoppingCart;
 
+
+
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
     public void tillbaka_varukorg_button_press() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("imat_app.fxml"));
         Stage window = (Stage) tillbaka_varukorg_button.getScene().getWindow();
         window.setScene(new Scene(root, 1000, 700));
-
+    }
+    public void gå_till_betalnining() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Betalning.fxml"));
+        Stage window = (Stage) betala_button.getScene().getWindow();
+        window.setScene(new Scene(root, 1000, 700));
     }
     public void varukorg_button_press() throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("varukorg_app.fxml"));
         Stage window = (Stage) varukorg_button.getScene().getWindow();
         window.setScene(new Scene(root, 1000, 700));
-
     }
+    public void fortsätt_till_kassan_button_press() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("personuppgifter.fxml"));
+        Stage window = (Stage) fortsätt_till_kassan.getScene().getWindow();
+        window.setScene(new Scene(root, 1000, 700));
+    }
+    public void Leverans_button_press() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Leveranstid.fxml"));
+        Stage window = (Stage) Leveransdetaljer.getScene().getWindow();
+        window.setScene(new Scene(root, 1000, 700));
+    }
+    public void Personuppgifter_button_press() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("personuppgifter.fxml"));
+        Stage window = (Stage) personuppgifter_button.getScene().getWindow();
+        window.setScene(new Scene(root, 1000, 700));
+    }
+    public void Spara_uppgifter() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Leveranstid.fxml"));
+        Stage window = (Stage) stäng_knappen.getScene().getWindow();
+        window.setScene(new Scene(root, 1000, 700));
+    }
+
+    
+
+
     public void initialize(URL url, ResourceBundle rb) {
 
         model.getShoppingCart().addShoppingCartListener(this);
 
         String iMatDirectory = iMatDataHandler.imatDirectory();
 
-        updateProductList(model.getProducts());
+        //updateProductList(model.getProducts());
 
         updateVarukorgList(model.getShoppingCart().getItems());
 
